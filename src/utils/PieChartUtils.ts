23267bbd11dcbd186508ad6239777cb6f5bed1e0
Cherkashin Alexander <cherkalexander@gmail.com>
2022-02-13 21:25:03 +0300

    🎨 Add PieChartDatum interface

--- src/utils/PieChartUtils.ts
+++ src/utils/PieChartUtils.ts
@@ -3,46 +3,60 @@ import { arrange, asc, groupBy, summarize, tidy, n } from '@tidyjs/tidy';
 import { PieSvgProps } from '@nivo/pie';
 import { BarDatum } from '@nivo/bar';
 
-export function convertToCommentsReceivedPieChart(comments: UserComment[]) {
+export interface PieChartDatum extends BarDatum {
+  id: string;
+  label: string;
+  value: number;
+}
+
+export function convertToCommentsReceivedPieChart(comments: UserComment[]): PieChartDatum[] {
   const rawData = getAuthorReviewerFromComments(comments);
-  const data = tidy(rawData, groupBy('author', [summarize({ total: n() })]), arrange([asc('total')])).map((item) => ({
-    id: item.author,
-    label: item.author,
-    value: item.total,
-  }));
+  const data = tidy(rawData, groupBy('author', [summarize({ total: n() })]), arrange([asc('total')])).map<PieChartDatum>(
+    (item) => ({
+      id: item.author,
+      label: item.author,
+      value: item.total,
+    })
+  );
 
   return data;
 }
 
-export function convertToCommentsLeftPieChart(comments: UserComment[]) {
+export function convertToCommentsLeftPieChart(comments: UserComment[]): PieChartDatum[] {
   const rawData = getAuthorReviewerFromComments(comments);
-  const data = tidy(rawData, groupBy('reviewer', summarize({ total: n() })), arrange([asc('total')])).map((item) => ({
-    id: item.reviewer,
-    label: item.reviewer,
-    value: item.total,
-  }));
+  const data = tidy(rawData, groupBy('reviewer', summarize({ total: n() })), arrange([asc('total')])).map<PieChartDatum>(
+    (item) => ({
+      id: item.reviewer,
+      label: item.reviewer,
+      value: item.total,
+    })
+  );
 
   return data;
 }
 
-export function convertToDiscussionsReceivedPieChart(discussions: UserDiscussion[]) {
+export function convertToDiscussionsReceivedPieChart(discussions: UserDiscussion[]): PieChartDatum[] {
   const rawData = getAuthorReviewerFromDiscussions(discussions);
-  const data = tidy(rawData, groupBy('author', [summarize({ total: n() })]), arrange([asc('total')])).map((item) => ({
-    id: item.author,
-    label: item.author,
-    value: item.total,
-  }));
+  const data = tidy(rawData, groupBy('author', [summarize({ total: n() })]), arrange([asc('total')])).map<PieChartDatum>(
+    (item) => ({
+      id: item.author,
+      label: item.author,
+      value: item.total,
+    })
+  );
 
   return data;
 }
 
-export function convertToDiscussionsStartedPieChart(discussions: UserDiscussion[]) {
+export function convertToDiscussionsStartedPieChart(discussions: UserDiscussion[]): PieChartDatum[] {
   const rawData = getAuthorReviewerFromDiscussions(discussions);
-  const data = tidy(rawData, groupBy('reviewer', summarize({ total: n() })), arrange([asc('total')])).map((item) => ({
-    id: item.reviewer,
-    label: item.reviewer,
-    value: item.total,
-  }));
+  const data = tidy(rawData, groupBy('reviewer', summarize({ total: n() })), arrange([asc('total')])).map<PieChartDatum>(
+    (item) => ({
+      id: item.reviewer,
+      label: item.reviewer,
+      value: item.total,
+    })
+  );
 
   return data;
 }
