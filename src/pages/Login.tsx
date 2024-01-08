b215e98e4bf9b107ab1e1e1856bc82cd986689d3
Cherkashin Alexander <cherkalexander@gmail.com>
2024-01-08 19:27:53 +0300

    feat(login): add hosting type field

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,19 +1,33 @@
-import { Stack, TextField, Typography } from '@mui/material';
+import {
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+  Stack,
+  TextField,
+  Typography,
+} from '@mui/material';
 import { Box } from '@mui/system';
 import { useCallback, useEffect, useState } from 'react';
 import { ReactComponent as GitLabIcon } from './../components/gitlab.svg';
+import { ReactComponent as GiteaIcon } from './../components/gitea.svg';
 import { TooltipPrompt } from '../components';
 import { getIsAuthenticated, useAuthStore } from '../stores/AuthStore';
 import { useNavigate } from 'react-router-dom';
 import { LoadingButton } from '@mui/lab';
 import { getCredentials } from '../utils/CredentialUtils';
+import { Logo } from '../components/Logo';
 
 export interface LoginProps {}
 
 export function Login(_: LoginProps) {
   const navigate = useNavigate();
+  const [selectedOption, setSelectedOption] = useState('Gitlab');
   const [token, setToken] = useState('');
   const [host, setHost] = useState('');
+  //TODO: upgrade zustand and use shallowEquals
   const { signIn, isSigningIn } = useAuthStore();
   const isAuthenticated = useAuthStore(getIsAuthenticated);
 
@@ -45,13 +59,37 @@ export function Login(_: LoginProps) {
     }
   }, [isAuthenticated, navigate, signIn]);
 
+  const handleChange = (event: SelectChangeEvent<string>) => {
+    setSelectedOption(event.target.value);
+  };
+
   return (
-    <Box style={{ width: '100%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+    <Box display="flex" justifyContent="center" alignItems="center" width="100%" height="100%">
       <Stack style={{ width: 500 }} spacing={2}>
-        <GitLabIcon style={{ width: 150, alignSelf: 'center' }} />
-        <Typography variant="h5" component="h1" alignSelf={'center'}>
-          GitLab Code Review Analyzer
-        </Typography>
+        <Stack alignItems={'center'} spacing={2}>
+          <Logo />
+          <Typography variant="h5" component="h1">
+            Code Review Analyzer
+          </Typography>
+        </Stack>
+        <Select required value={selectedOption} onChange={handleChange} style={{ height: 56 }}>
+          <MenuItem value="Gitlab">
+            <ListItem>
+              <ListItemIcon>
+                <GitLabIcon style={{ width: 24 }} />
+              </ListItemIcon>
+              <ListItemText>Gitlab</ListItemText>
+            </ListItem>
+          </MenuItem>
+          <MenuItem value="Gitea">
+            <ListItem>
+              <ListItemIcon>
+                <GiteaIcon style={{ width: 24 }} />
+              </ListItemIcon>
+              <ListItemText>Gitea</ListItemText>
+            </ListItem>
+          </MenuItem>
+        </Select>
         <TextField
           required
           label="Host"
