6789f2ff22a7c369fe553f7118f06b2790588b9f
Cherkashin Alexander <cherkalexander@gmail.com>
2024-02-13 11:11:57 +0300

    refactor: create separate component for CommentsTooltip

--- src/components/charts/CommentsPerMonthChart/CommentsPerMonthChart.tsx
+++ src/components/charts/CommentsPerMonthChart/CommentsPerMonthChart.tsx
@@ -4,6 +4,7 @@ import { Comment } from '../../../clients/types';
 import { useMemo, useReducer, useState } from 'react';
 import { convertToCommentsLineChart } from './CommentsPerMonthChartUtils';
 import { SquareMarker } from '../../BaseChartTooltip';
+import { SliceTooltipProps } from '@nivo/line';
 
 export interface CommentsPerMonthChartProps {
   comments: Comment[];
@@ -20,29 +21,30 @@ export function CommentsPerMonthChart({ comments }: CommentsPerMonthChartProps) 
         legendYLabel="Comments count"
         data={data}
         onLegendClick={(selected) => setSelectedAuthors(selected.id as string)}
-        sliceTooltip={({ slice }) => {
-          return (
-            <div
-              style={{
-                background: 'white',
-                padding: '9px 12px',
-                border: '1px solid #ccc',
-              }}
-            >
-              {slice.points
-                .toSorted((a, b) => parseInt(b.data.yFormatted.toString()) - parseInt(a.data.yFormatted.toString()))
-                .map((point) => (
-                  <div key={point.id} style={{ padding: '3px 0' }}>
-                    <SquareMarker color={point.serieColor} />
-                    <span>{point.serieId}</span>{' '}
-                    <strong style={{ float: 'right', marginLeft: 16 }}>{point.data.yFormatted}</strong>
-                  </div>
-                ))}
-            </div>
-          );
-        }}
+        sliceTooltip={CommentsTooltip}
       />
     </ChartContainer>
+  );
+}
+
+function CommentsTooltip({ slice }: SliceTooltipProps) {
+  return (
+    <div
+      style={{
+        background: 'white',
+        padding: '9px 12px',
+        border: '1px solid #ccc',
+      }}
+    >
+      {slice.points
+        .toSorted((a, b) => parseInt(b.data.yFormatted.toString()) - parseInt(a.data.yFormatted.toString()))
+        .map((point) => (
+          <div key={point.id} style={{ padding: '3px 0' }}>
+            <SquareMarker color={point.serieColor} />
+            <span>{point.serieId}</span> <strong style={{ float: 'right', marginLeft: 16 }}>{point.data.yFormatted}</strong>
+          </div>
+        ))}
+    </div>
   );
 }
 
