b3be6b9d4a4136ce46597d9317f09f96b6967613
Cherkashin Alexander <cherkalexander@gmail.com>
2024-02-19 21:11:04 +0300

    fix: fix line chart tooltip
    incorrect values weree shown, need to use y instead of yFormatted property

--- src/components/charts/CommentsPerMonthChart/CommentsPerMonthChart.tsx
+++ src/components/charts/CommentsPerMonthChart/CommentsPerMonthChart.tsx
@@ -37,11 +37,11 @@ function CommentsTooltip({ slice }: SliceTooltipProps) {
       }}
     >
       {slice.points
-        .toSorted((a, b) => parseInt(b.data.yFormatted.toString()) - parseInt(a.data.yFormatted.toString()))
+        .toSorted((a, b) => parseInt(b.data.y.toString()) - parseInt(a.data.y.toString()))
         .map((point) => (
           <div key={point.id} style={{ padding: '3px 0' }}>
             <SquareMarker color={point.serieColor} />
-            <span>{point.serieId}</span> <strong style={{ float: 'right', marginLeft: 16 }}>{point.data.yFormatted}</strong>
+            <span>{point.serieId}</span> <strong style={{ float: 'right', marginLeft: 16 }}>{point.data.y.toString()}</strong>
           </div>
         ))}
     </div>
