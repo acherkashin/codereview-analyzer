dad04fab9c36ba3bec73484bb5bf0be0a94dbe95
Cherkashin Alexander <cherkalexander@gmail.com>
2024-03-02 15:11:25 +0300

    feat(ApprovalDistributionChart): add tooltip for ApprovalDistributionChart - All

--- src/components/charts/ApprovalDistributionChart/ApprovalDistributionChart.tsx
+++ src/components/charts/ApprovalDistributionChart/ApprovalDistributionChart.tsx
@@ -4,6 +4,8 @@ import { getWhomUserApproves } from './ApprovalDistributionUtils';
 import { BarChart } from '../BarChart';
 import { ChartContainer } from '../../ChartContainer';
 import { getBarChartData } from '../../../utils/ChartUtils';
+import { BarDatum, BarTooltipProps } from '@nivo/bar';
+import { BaseChartTooltip } from '../../BaseChartTooltip';
 
 export interface ApprovalDistributionProps {
   user?: User | null;
@@ -13,7 +15,7 @@ export interface ApprovalDistributionProps {
 }
 
 /**
- * Represents who gives approvals
+ * Represents who gives approvals.
  */
 export function ApprovalDistributionChart(props: ApprovalDistributionProps) {
   if (props.user) return <ApprovalDistributionForUser {...props} />;
@@ -21,25 +23,45 @@ export function ApprovalDistributionChart(props: ApprovalDistributionProps) {
   return <ApprovalDistributionForAll {...props} />;
 }
 
+/**
+ * On y-axis - who gives approvals,
+ * On x-axis - how many approvals is given.
+ * Inside every bar is visible to whom approvals were given.
+ */
 function ApprovalDistributionForAll({ pullRequests, users }: ApprovalDistributionProps) {
   const { data, authors } = useMemo(() => {
     return getBarChartData(pullRequests, users, getWhomUserApproves);
   }, [users, pullRequests]);
 
   return (
-    <ChartContainer title="Approvals Given By">
+    <ChartContainer title="Approved By">
       <BarChart
         margin={{ left: 100, bottom: 50, right: 30 }}
         // axisBottom={{}}
         indexBy="approverName"
         keys={authors}
         data={data}
+        tooltip={ApprovalsAllTooltip}
         onClick={() => {}}
       />
     </ChartContainer>
   );
 }
 
+const ApprovalsAllTooltip: Parameters<typeof BarChart>[0]['tooltip'] = (props: BarTooltipProps<BarDatum>) => {
+  const { indexValue: approver, id: author, value: approvesCount } = props;
+
+  return (
+    <BaseChartTooltip>
+      <strong>{approver}</strong> approved <strong>{approvesCount}</strong> pull requests of <strong>{author}</strong>
+    </BaseChartTooltip>
+  );
+};
+
+/**
+ * On y-axis - who receives approvals from @user
+ * On x-axis - how many approvals are received
+ */
 function ApprovalDistributionForUser({ pullRequests, user }: ApprovalDistributionProps) {
   const data = useMemo(() => {
     const obj = getWhomUserApproves(pullRequests, user!.id);
