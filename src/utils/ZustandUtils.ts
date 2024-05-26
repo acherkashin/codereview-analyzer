0440b49ea82e385efccef3962a3bf614940cf3dc
Cherkashin Alexander <cherkalexander@gmail.com>
2024-05-26 18:37:41 +0300

    chore: add comment

--- src/utils/ZustandUtils.ts
+++ src/utils/ZustandUtils.ts
@@ -3,5 +3,6 @@ import { devtools } from 'zustand/middleware';
 
 //https://www.youtube.com/watch?v=o4UTnrXmgac&t=10s&ab_channel=HolyJS
 export const createStore = <T>(fn: Parameters<typeof devtools<T>>[0], name: string) => {
+  // ⛔️ If too many pull requests are loaded it will slow down the application, if you enable redux devtools
   return create<T>()(devtools(fn, { name, enabled: process.env.NODE_ENV !== 'production' }));
 };
