4822f7ad1d93fec4e3d14853f96d7a09bc403a96
Cherkashin Alexander <cherkalexander@gmail.com>
2025-02-11 19:00:54 +0300

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,8 @@ import svgr from 'vite-plugin-svgr';
 // https://vite.dev/config/
 export default defineConfig({
   base: '/codereview-analyzer/',
+  build: {
+    outDir: 'build',
+  },
   plugins: [react(), svgr()],
 });
